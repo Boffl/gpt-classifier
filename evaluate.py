c388168e7823c86cafea1bbcb2104a65e05f4aa4
nikob <nikolaj.bauer@uzh.ch>
2023-11-10 14:19:37 +0100

    make generalized for all tasks and make CLI

--- evaluate.py
+++ evaluate.py
@@ -1,25 +1,35 @@
-def preprocess_predictions(file):
-    classes = ['anger', 'joy', 'optimism', 'sadness']
+from argparse import ArgumentParser
+
+def get_enc_labels(filepath, mapping):
+    '''turn the gpt generated classes into encoded labels'''
+    print(mapping)
     predictions = []
-    with open(file) as infile:
+    with open(filepath) as infile:
         for line in infile:
-            for cl in classes:
-                if line.startswith(cl):
-                    predictions.append(cl)
-    mapping = {'anger': 0, 'joy': 1, 'optimism': 2, 'sadness': 3}
-    predictions = [ mapping[ pred ] for pred in predictions ]
-    for pred in predictions:
-        print(pred)
-        with open('outfile.txt', 'a', encoding='utf-8') as writer:
-            writer.write(f'{pred}\n')
+            for label in mapping:
+                if line.startswith(label):
+                    predictions.append(label)
+
+    return [mapping[pred] for pred in predictions]
 
 
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('infile')
+    parser.add_argument('mappingfile')
+    parser.add_argument('outfile')
+    args = parser.parse_args()
 
+    mapping = {}
+    with open(args.mappingfile, 'r', encoding='utf-8') as reader:
+        for line in reader:
+            label_enc, label_text = line.split()[0], line.split()[1].strip()
+            mapping[label_text] = label_enc
 
-
-
-
-preds = preprocess_predictions('predictions/emotion/test_1.txt')
+    preds = get_enc_labels(args.infile, mapping)
+    with open(args.outfile, 'w', encoding='utf-8') as outfile:
+        for pred in preds:
+            outfile.write(f'{pred}\n')
 
 
 
