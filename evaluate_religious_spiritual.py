0c9b9fa5c348cdae1e9edae8fbe1e6f45fab1a99
moprei21 <62673995+moprei21@users.noreply.github.com>
2023-12-04 13:50:12 +0100

    add class probabilities and comparison

--- evaluate_religious_spiritual.py
+++ evaluate_religious_spiritual.py
@@ -25,26 +25,55 @@ def get_mapping(task):
 
 
 def compare_spirit_religion(task):
+    total_spirit = 930
+    total_religious = 1067
+
     spiritual_file = f'predictions_zero_shot/spirituality_{task}.txt'
     religious_file = f'predictions_zero_shot/religion_{task}.txt'
 
-    spiritual_result = eval_data(task, spiritual_file).most_common()
-    religious_result = eval_data(task, religious_file).most_common()
+    spiritual_result = eval_data(task, spiritual_file)
+    religious_result = eval_data(task, religious_file)
 
-    print(f' Spiritual results: {spiritual_result}')
-    print(f' Religious_result: {religious_result}')
+    class_percentages_spirit = {key: (count / total_spirit) for key, count in spiritual_result.items()}
+    class_percentages_religious = {key: (count / total_religious) for key, count in religious_result.items()}
+
+    mapping = get_mapping(task)
+
+    for maps in mapping.values():
+        print(f' {maps} : Religious = {class_percentages_religious[maps]:.3f} , Spiritual = {class_percentages_spirit[maps]:.3f}')
 
 
 
 
-
+   # print(f' Spiritual results: {spiritual_result}')
+   # print(f' Religious_result: {religious_result}')
 
 
 
 
 def main():
-    task = 'sentiment'
 
-    compare_spirit_religion(task)
+    print('HATE SUBTASK')
+    compare_spirit_religion('hate')
+
+    print('-'*77)
+
+    print('OFFENSIVE SUBTASK')
+    compare_spirit_religion('offensive')
+    print('-' * 77)
+
+    print('EMOTION')
+    compare_spirit_religion('emotion')
+    print('-' * 77)
+
+    print('SENTIMENT SUBTASK')
+    compare_spirit_religion('sentiment')
+
+
+
+
+
+
+
 if __name__ == '__main__':
     main()
