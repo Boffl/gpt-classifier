74d20d846692acd280fb885b182c0862ff8590ec
nikob <nikolaj.bauer@uzh.ch>
2023-12-25 10:49:00 +0100

    iterate recursively, to calculate tototal costs of all experiments

--- calculate_cost.py
+++ calculate_cost.py
@@ -6,12 +6,13 @@ price_prompt = 0.001
 price_gen = 0.002
 
 cost = 0
-for filename in os.listdir(infolder):
-    filepath = os.path.join(infolder, filename)
+for subdir, dirs, filenames in os.walk(infolder):
+    for filename in filenames:
+        filepath = os.path.join(subdir, filename)
 
-    with open(filepath, 'r', encoding='utf-8') as infile:
-        for line in infile:
-            prompt_toks, gen_toks = line.split(',')[0], line.split(',')[1].strip()
+        with open(filepath, 'r', encoding='utf-8') as infile:
+            for line in infile:
+                prompt_toks, gen_toks = line.split(',')[0], line.split(',')[1].strip()
 
-            cost += 0.001*int(prompt_toks)*price_prompt + 0.001*int(gen_toks)*price_gen
+                cost += 0.001*int(prompt_toks)*price_prompt + 0.001*int(gen_toks)*price_gen
 print(cost)
