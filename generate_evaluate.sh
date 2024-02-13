ee8dbf9740879fb36649f7911c8ca213459431f1
nikob <nikolaj.bauer@uzh.ch>
2024-02-13 19:37:09 +0100

    add option with the input prompt

--- generate_evaluate.sh
+++ generate_evaluate.sh
@@ -3,22 +3,30 @@
 task=$1
 shots=$2  # zero_shot or 12_shot or what not
 n=$3  # number of tweets for the progress bar
-if [ -z "$4" ]; then
-    model="gpt-3"
+model=$4
+if [ -z "$5" ]; then
+    prompt="You are a helpful assistant, tasked with classifying the user input according to following classes: "
+    prompt_dir=""
 else
-    model="$4"
+    prompt="$5"
+    prompt_dir="specific_prompt/"
 fi
+
+responses_dir="${model}_responses/${shots}/${prompt_dir}"
+predictions_dir="${model}_predictions/${shots}/${prompt_dir}"
 
 # call the generate script
 if [ "$shots" = "zero_shot" ]; then
 
-  python3.11 gpt-classifier.py "raw_data/${task}/test_text.txt" "raw_data/${task}/classes.txt" "${model}_responses/${shots}/${task}.txt" \
-   --count_tokens "api_usage_log/${model}/${task}_${shots}.csv" --number_of_tweets $n --model_name "${model}"
+  python3.11 gpt-classifier.py "raw_data/${task}/test_text.txt" "raw_data/${task}/classes.txt" "${responses_dir}${task}.txt" \
+   --count_tokens "api_usage_log/${model}/${prompt_dir}${task}_${shots}.csv" --number_of_tweets $n --model_name "${model}" \
+   --prompt "${prompt}"
 else
-    python3.11 gpt-classifier.py "raw_data/${task}/test_text.txt" "raw_data/${task}/classes.txt" "${model}_responses/${shots}/${task}.txt" \
-   --few_shot "raw_data/${task}/examples.jsonl" --count_tokens "api_usage_log/${model}/${task}_${shots}.csv" --number_of_tweets $n --model_name "${model}"
+    python3.11 gpt-classifier.py "raw_data/${task}/test_text.txt" "raw_data/${task}/classes.txt" "${responses_dir}${task}.txt" \
+   --few_shot "raw_data/${task}/examples.jsonl" --count_tokens "api_usage_log/${model}/${prompt_dir}${task}_${shots}.csv" --number_of_tweets $n \
+    --model_name "${model}" --prompt "${prompt}"
 fi
 
-python3.11 encode_predictions.py "${model}_responses/${shots}/${task}.txt" "raw_data/${task}/mapping.txt" "${model}_predictions/${shots}/${task}.txt"
+python3.11 encode_predictions.py "${responses_dir}${task}.txt" "raw_data/${task}/mapping.txt" "${predictions_dir}${task}.txt"
 
-python3.11 evaluate_script.py --tweeteval_path "raw_data/" --predictions_path "${model}_predictions/${shots}/" --task "${task}"
+python3.11 evaluate_script.py --tweeteval_path "raw_data/" --predictions_path "${predictions_dir}" --task "${task}"
