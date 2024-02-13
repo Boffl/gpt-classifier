1aa8297c53bcc79e483afe148b0a7755abe60671
nikob <nikolaj.bauer@uzh.ch>
2024-02-13 16:54:43 +0100

    add arguments to input the pricing and an option to only calculate costs for one single file

--- calculate_cost.py
+++ calculate_cost.py
@@ -2,17 +2,36 @@ import sys
 import os
 
 infolder = sys.argv[1]
-price_prompt = 0.001
-price_gen = 0.002
+# price per 1k tokens
+price_prompt = float(sys.argv[2])
+price_gen = float(sys.argv[3])
+
+# # Prices last year for gpt-3.5-turbo
+# price_prompt = 0.001
+# price_gen = 0.002
+
+
+def count_cost_per_file(filepath):
+    global cost
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        for line in infile:
+            prompt_toks, gen_toks = line.split(',')[0], line.split(',')[1].strip()
+
+            cost += 0.001 * int(prompt_toks) * price_prompt + 0.001 * int(gen_toks) * price_gen
+
 
 cost = 0
-for subdir, dirs, filenames in os.walk(infolder):
-    for filename in filenames:
-        filepath = os.path.join(subdir, filename)
+if os.path.isdir(infolder):
+    for subdir, dirs, filenames in os.walk(infolder):
+        for filename in filenames:
+            filepath = os.path.join(subdir, filename)
+            count_cost_per_file(filepath)
 
-        with open(filepath, 'r', encoding='utf-8') as infile:
-            for line in infile:
-                prompt_toks, gen_toks = line.split(',')[0], line.split(',')[1].strip()
+elif os.path.isfile(infolder):
+    count_cost_per_file(infolder)
 
-                cost += 0.001*int(prompt_toks)*price_prompt + 0.001*int(gen_toks)*price_gen
+else:
+    print(f"{infolder} is neither a filepath or directory")
+    exit(1)
+
 print(cost)
